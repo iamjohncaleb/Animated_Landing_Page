662b2ff5c11483ab1baecd1fec3001cc72726a4c
Caleb John <johncaleb022@gmail.com>
2024-02-12 00:18:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let hill5 = document.getElementById('hill5');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    
     text.style.marginTop = value * 2.5 + 'px';
     leaf.style.top = value * -1.5 + 'px';
     leaf.style.left = value * 1.5 + 'px';
